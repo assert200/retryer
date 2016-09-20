05385a1099122f593a886ddca973e421991024d1
James Barker <james.barker@auspost.com.au>
2016-09-20 20:15:19 +1000

    initial commit

---
+++ lib/retryer.rb
@@ -0,0 +1,57 @@
+module Retryer
+  class Retry
+    attr_accessor :max_retries, :interval
+
+    def initialize(max_retries: 5, interval: 1, verbose: true)
+      @max_retries=max_retries
+      @interval=interval
+      @verbose=verbose
+    end
+
+    def until(description: 'Operation')
+      retries = 0
+      begin
+        result = yield
+        raise "Condition not met" unless result
+      rescue Exception => e
+        if retries < @max_retries
+          sleep @interval
+          retries += 1
+          puts "Retrying: '#{description}' (retries #{retries}) with caught exeception: #{e}" if @verbose
+          retry
+        end
+        raise "MAX RETRIES REACHED: '#{description}' failed after retrying #{@max_retries} times."
+      end
+    end
+  end
+
+  class Wait
+    attr_accessor :timeout, :interval
+
+    def initialize(timeout: 5, interval: 1, verbose: true)
+      @timeout=timeout
+      @interval=interval
+      @verbose=verbose
+    end
+
+    def until(description: 'Operation')
+      begin
+        Timeout::timeout(@timeout) do
+          retries = 0
+          begin
+            result = yield
+            raise "Condition not met" unless result
+          rescue Exception => e
+            sleep @interval
+            retries += 1
+            puts "Retrying during wait (retries #{retries}) '#{description}' with caught exeception: #{e}" if @verbose
+            retry
+          end
+        end
+
+      rescue Exception => e
+        raise "TIMEOUT: '#{description}' after waiting #{@timeout} seconds."
+      end
+    end
+  end
+end
---
+++ retryer.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'retryer'
+  s.version     = '0.1.0'
+  s.date        = '2016-09-20'
+  s.summary     = "Retryer"
+  s.description = "Retry and wait tool"
+  s.authors     = ["James Barker"]
+  s.email       = 'jarbarker@gmail.com'
+  s.files       = ["lib/retryer.rb"]
+  s.homepage    =
+    'https://github.com/assert200/retryer'
+  s.license       = 'MIT'
+end
